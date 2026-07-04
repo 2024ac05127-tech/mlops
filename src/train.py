07deab9cf910a2c4f3ceb5351599fe5ccd29d64d
2024ac05127-tech <2024ac05127@wilp.bits-pilani.ac.in>
2026-07-04 20:38:58 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -11,13 +11,13 @@ X = df.drop("num", axis=1)
 y = df["num"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X,y,test_size=0.2,random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
 model = RandomForestClassifier()
 
-model.fit(X_train,y_train)
+model.fit(X_train, y_train)
 
-joblib.dump(model,"heart_disease_model.pkl")
+joblib.dump(model, "heart_disease_model.pkl")
 
 print("Model Saved")
