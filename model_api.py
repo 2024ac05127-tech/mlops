e10ba46083ea337734c695f3634b031b6f046e03
Krishna Priya R <krishnareghu2018@gmail.com>
2026-07-05 22:17:54 +0530

    added api files

---
+++ model_api.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("heart_disease_model.pkl")
+
+
+class HeartData(BaseModel):
+    age: float
+    sex: float
+    cp: float
+    trestbps: float
+    chol: float
+    fbs: float
+    restecg: float
+    thalach: float
+    exang: float
+    oldpeak: float
+    slope: float
+    ca: float
+    thal: float
+
+
+@app.post("/predict")
+def predict(data: HeartData):
+
+    features = np.array([
+        [
+            data.age,
+            data.sex,
+            data.cp,
+            data.trestbps,
+            data.chol,
+            data.fbs,
+            data.restecg,
+            data.thalach,
+            data.exang,
+            data.oldpeak,
+            data.slope,
+            data.ca,
+            data.thal
+        ]
+    ])
+
+    prediction = model.predict(features)[0]
+
+    confidence = float(max(model.predict_proba(features)[0]))
+
+    return {
+        "prediction": int(prediction),
+        "confidence": round(confidence, 4)
+    }
